5e569a031adefb6d1ed118ac78a6b6a77ba33b6f
Menno Schellekens <mhschel@gmail.com>
2020-04-26 15:11:03 +0200

    clean attendance list

--- check_names.py
+++ check_names.py
@@ -1,5 +1,11 @@
-master_list_file = open('master_list', 'r')
-master_list = master_list_file.readlines()
+def read_names(file_path: str) -> list:
+    names_file = open(file_path, 'r')
+    names_list = names_file.readlines()
+    names_list = [name[:-1] for name in names_list]
+    return names_list
 
-master_list = [name[:-2] for name in master_list]
-print(master_list)
+
+master_list = read_names('master_list')
+
+attendance_list = read_names('sessions/save-users-list-1587463546037.txt')
+print(attendance_list)
